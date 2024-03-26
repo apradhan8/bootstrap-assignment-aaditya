58e67609bd0f3da23eec02d9fc5a8a7a1f6b3d46
Nabin Dahal <67369378+Nabin-me@users.noreply.github.com>
2024-03-26 03:19:06 -0400

    navbar touchup

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,20 @@
 // JavaScript to handle sidebar toggling
 document.addEventListener('DOMContentLoaded', function () {
     const sidebar = document.querySelector('.sidebar');
-    const expandButton = document.getElementById('expand-btn');
+    const expandButton = document.getElementById('logo-btn');
     const sidebarLinks = document.querySelectorAll('.nav-link');
-    const navLinks = document.querySelectorAll('.nav-link');
+    const mainContent = document.querySelector('.content');
+    const mediaQueryList = window.matchMedia('(max-width: 992px)');
+
+    mediaQueryList.addEventListener('change', function (event) {
+        if (event.matches) {
+            sidebar.classList.add('collapsed');
+            mainContent.classList.add('collapsed');
+        } else {
+            sidebar.classList.remove('collapsed');
+            mainContent.classList.remove('collapsed');
+        }
+    })
 
     sidebarLinks.forEach(function (link) {
         link.addEventListener('click', function () {
@@ -19,8 +30,85 @@ document.addEventListener('DOMContentLoaded', function () {
 
     expandButton.addEventListener('click', function () {
         sidebar.classList.toggle('collapsed');
+        mainContent.classList.toggle('collapsed');
     });
 
+    var options = {
+        chart: {
+            type: 'line',
+            toolbar: {
+                show: false,
+
+            }
+        },
+        series: [{
+            name: 'Yesterday',
+            data: [0, 10, 20, 30, 40, 50, 60]
+        },
+        {
+            name: 'Today',
+            data: [10, 20, 30, 40, 50, 60, 70]
+        }],
+        xaxis: {
+            categories: [0, 1, 2, 3, 4, 5, 6],
+        },
+        yaxis: [{
+            axisTicks: {
+                show: true,
+            },
+            axisBorder: {
+                show: true,
+                color: '#008FFB'
+            },
+            labels: {
+                style: {
+                    colors: '#008FFB',
+                }
+            },
+            title: {
+                text: "Yesterday",
+                style: {
+                    color: '#008FFB',
+                }
+            },
+            tooltip: {
+                enabled: true
+            }
+        },
+        {
+            opposite: true,
+            axisTicks: {
+                show: true,
+            },
+            axisBorder: {
+                show: true,
+                color: '#00E396'
+            },
+            labels: {
+                style: {
+                    colors: '#00E396',
+                }
+            },
+            title: {
+                text: "Today",
+                style: {
+                    color: '#00E396',
+                }
+            },
+            tooltip: {
+                enabled: true
+            }
+        }],
+        legend: {
+            position: 'top',
+            horizontalAlign: 'center'
+        }
+    };
+
+    var chart = new ApexCharts(document.querySelector(".chart"), options);
+
+    chart.render();
 
 
 });
+

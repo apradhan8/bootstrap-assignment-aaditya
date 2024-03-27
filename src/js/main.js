3313493c68ee915f0041446188830b67354b66e2
Nabin Dahal <67369378+Nabin-me@users.noreply.github.com>
2024-03-26 21:33:06 -0400

    fixed: styling issues and redundant code

--- src/js/main.js
+++ src/js/main.js
@@ -34,80 +34,156 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     var options = {
+        series: [{
+            name: "Today",
+            data: [{
+                x: 0,
+                y: 12
+            }, {
+                x: 3,
+                y: 30
+            }, {
+                x: 5,
+                y: 28
+            }, {
+                x: 8,
+                y: 50
+            }, {
+                x: 11,
+                y: 18
+            }, {
+                x: 14.5,
+                y: 38
+            },
+            {
+                x: 16,
+                y: 47
+            },
+            {
+                x: 19,
+                y: 38
+            },
+            ],
+        },
+        {
+            name: 'Yesterday',
+            data: [{
+                x: 0,
+                y: 35
+            }, {
+                x: 4.5,
+                y: 22
+            }, {
+                x: 5,
+                y: 28
+            }, {
+                x: 8,
+                y: 32
+            }, {
+                x: 13,
+                y: 18
+            }, {
+                x: 15.5,
+                y: 42
+            },
+            {
+                x: 16.5,
+                y: 29.5
+            },
+            {
+                x: 19,
+                y: 36
+            },
+            ],
+
+
+        }
+        ],
         chart: {
-            type: 'line',
+            type: "area",
             toolbar: {
                 show: false,
+            },
+            animations: {
+                enabled: true,
+                easing: 'easeinout',
+                speed: 800,
+                animateGradually: {
+                    enabled: true,
+                    delay: 150
+                },
+                dynamicAnimation: {
+                    enabled: true,
+                    speed: 350
+                }
+            }
 
-            }
         },
-        series: [{
-            name: 'Yesterday',
-            data: [0, 10, 20, 30, 40, 50, 60]
+        dataLabels: {
+            enabled: false
         },
-        {
-            name: 'Today',
-            data: [10, 20, 30, 40, 50, 60, 70]
-        }],
         xaxis: {
-            categories: [0, 1, 2, 3, 4, 5, 6],
-        },
-        yaxis: [{
-            axisTicks: {
-                show: true,
-            },
-            axisBorder: {
-                show: true,
-                color: '#008FFB'
-            },
+            type: "numeric",
+            min: 0,
+            max: 22,
+            tickAmount: 23,
             labels: {
-                style: {
-                    colors: '#008FFB',
+                formatter: function (val) {
+                    return Math.round(val);
                 }
             },
-            title: {
-                text: "Yesterday",
-                style: {
-                    color: '#008FFB',
-                }
-            },
-            tooltip: {
-                enabled: true
+            axisTicks: {
+                show: false
             }
         },
-        {
+        yaxis: {
+            min: 0,
+            max: 60,
             opposite: true,
-            axisTicks: {
-                show: true,
-            },
-            axisBorder: {
-                show: true,
-                color: '#00E396'
-            },
-            labels: {
-                style: {
-                    colors: '#00E396',
-                }
-            },
-            title: {
-                text: "Today",
-                style: {
-                    color: '#00E396',
-                }
-            },
-            tooltip: {
-                enabled: true
+        },
+        stroke: {
+            curve: "smooth",
+            width: 2
+        },
+        fill: {
+            type: "gradient",
+            gradient: {
+                shadeIntensity: 1,
+                inverseColors: !1,
+                opacityFrom: .12,
+                opacityTo: 0,
+                stops: [0, 90, 100]
             }
-        }],
+        },
+        colors: ["var(--bs-primary)", "var(--bs-gray-400)"],
+
+        tooltip: {
+            theme: "dark",
+            x: {
+                show: false
+            },
+            y: {
+                title: {
+                    formatter: function () {
+                        return '';
+                    }
+                }
+            },
+        },
         legend: {
-            position: 'top',
-            horizontalAlign: 'center'
+            show: true,
+            position: "top",
+            horizontalAlign: "right",
+
         }
+
     };
 
     var chart = new ApexCharts(document.querySelector(".chart"), options);
 
     chart.render();
+
+
 
 
 });
